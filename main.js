4aaf063bafa7eb4388f0c84e7bfd82af8dc925f0
Jodi Fogle <jodifogle@yahoo.com>
2023-02-16 14:51:48 -0500

    conditionals

---
+++ main.js
@@ -0,0 +1,73 @@
+if(1+1 >4){
+    console.log("True Fact!");
+  } else {
+    console.log("Lies.")
+  }
+  if( 4*5<=20){
+    console.log("True Fact!");
+  } else {
+    console.log("Lies.")
+  }
+  if( 6-2>=0){
+    console.log("True Fact!");
+  } else {
+    console.log("Lies.")
+  }
+
+
+
+
+
+  var likesDogs ="true";
+
+
+  if (likesDogs === "true"){
+    console.log("Your a dog person!");
+  } else if (likesDogs === "false"){
+  console.log("your a cat");}
+
+
+//   var fullMoon = true;
+//   var isWolf = false;
+  
+//   // This block of code will only run if the moon is full AND you're a wolf. Right now, this code won't run because the value of isWolf is false.
+  
+//   if(fullMoon && isWolf){
+//     console.log("AWOOOOOOOOOOO");
+//   }
+  
+//   // What if we want non-wolves to be able to howl at the full moon too? This block will run if you're a wolf OR if the moon is full. However, if both conditions are false (you're not a wolf and the moon is not full), the code will not run.
+  
+//   if(fullMoon || isWolf){
+//     console.log("AWOOOOOOOOOOO");
+
+var firstName = "Jodi"
+  var preferredLanguage ="French"
+
+  if (firstName=== "English"){
+    console.log (`Hello, ${firstName}!`);
+  } else if (preferredLanguage === "Spanish"){
+    console.log (`Hola, ${firstName}!`);
+  } else if (preferredLanguage === "French"){
+    console.log (`Bonjour, ${firstName}!`);
+  }
+
+  var grade = "96"
+  if (grade >=0 && grade <= 69){
+    console.log ("you got an f");
+  }
+  if (grade >=70 && grade <= 76){
+    console.log ("you got an d");
+  }
+
+if (grade >=77 && grade <= 84){
+  console.log ("you got an c");
+}
+
+if (grade >=84 && grade <= 92){
+  console.log ("you got an b");
+}
+
+if (grade >=93 && grade <= 100){
+  console.log ("you got an a");
+}
